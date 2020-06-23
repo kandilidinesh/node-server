c455b4e551e4451c3d774db27a49502e6c50b204
kandilidinesh <kandilidinesh@gmail.com>
2020-06-23 21:13:47 +0530

    Added a HTML chunk response

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 // Callback Server Function
 const server = http.createServer((req, res) => {
-    console.log("Request Recieved");
+    console.log(req.url, req.method, req.headers); 
+    //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html> <title> My First Page</title> <body> <h1> Hello World!</h1> </body></html>');
+    res.end();
 });
 
 server.listen(3000);
