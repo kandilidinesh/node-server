36bb6ede1fe1e3fb4b27cb4941900b62e3853569
kandilidinesh <kandilidinesh@gmail.com>
2020-06-29 10:05:57 +0530

    Added & exported a module to perform operations.

---
+++ routes.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method; 
+    if(url === '/'){
+        // res.write('<html> <head><title>Store Data</title></head> <body> <form action="/message" method="POST"> Enter the Data: <input type="text" name="" id=""> <button type="submit">Save on server</button></form></body></html>');
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message">');
+        res.write('<button type="submit"> Save on Server</button>');
+        res.write('</form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', (buffer) => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('Message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+};
+
+module.exports = requestHandler;
